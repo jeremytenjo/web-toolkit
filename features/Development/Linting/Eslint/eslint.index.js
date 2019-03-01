2172bed6c621724e260e2975675477a84d9c4e4b
Jeremy <tenjojeremy@gmail.com>
2019-03-01 08:45:17 -0800

    .

---
+++ features/Development/Linting/Eslint/eslint.index.js
@@ -0,0 +1,47 @@
+module.exports = {
+  globals: {
+    MyGlobal: true,
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'babel', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect',
+    },
+    propWrapperFunctions: ['forbidExtraProps'],
+  },
+  rules: {
+    'no-const-assign': 'warn',
+    'no-this-before-super': 'warn',
+    'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-unused-vars': 'warn',
+    'no-debugger': 'warn',
+    'no-console': 'off',
+    'constructor-super': 'warn',
+    'valid-typeof': 'warn',
+    'react/display-name': 'off',
+    'react/jsx-uses-react': 1,
+    'react/prop-types': 'off',
+    'react/jsx-fragments': ['off', 'syntax'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
