e973e068c4c83b64f2f9337490a9b7d85713de86
Jeremy <tenjojeremy@gmail.com>
2019-04-28 19:40:23 -0700

    navbar center background

--- features/Navigation/Navbar/Component/navBar.styles.js
+++ features/Navigation/Navbar/Component/navBar.styles.js
@@ -79,11 +79,9 @@ export const MainIconCon = styled.div`
 
 export const Background = styled.svg`
   position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   z-index: -1;
   filter: drop-shadow(0px -4px 14px rgba(0, 0, 0, 0.06));
 `
