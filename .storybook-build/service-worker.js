03b26ee45042ab88aab6f7fc0979b84cfff50b4c
Jeremy <tenjojeremy@gmail.com>
2019-05-05 18:46:10 -0700

    add storybook build ignore

--- .storybook-build/service-worker.js
+++ .storybook-build/service-worker.js
@@ -11,23 +11,29 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js',
-)
+  "precache-manifest.cefc4e4881944e2722ac06653733b3fe.js"
+);
 
-importScripts('precache-manifest.45ad8a65c5d5639e6ea683a538d56db7.js')
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
-workbox.clientsClaim()
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute('/index.html', {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-})
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
