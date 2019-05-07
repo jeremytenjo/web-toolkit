e02575a45cdbae96a48718a295428d9a7c97e40f
Jeremy <tenjojeremy@gmail.com>
2019-05-06 18:34:47 -0700

    removed form resets theme

--- .storybook/Theme/Head/theme.head.js
+++ .storybook/Theme/Head/theme.head.js
@@ -1,5 +1,4 @@
 const { typographyResets } = require('../Typography/typography.resets')
-const { formResets } = require('../Forms/forms.resets')
 const { colorVars } = require('../Colors/colors.vars')
 const { selection } = require('../Browser/browser.selection')
 const { body } = require('../Browser/browser.body')
@@ -10,7 +9,6 @@ const { spacing } = require('../Spacing/spacing.index')
 
 exports.headCss = `
 <style type="text/css">
-${formResets}
 ${typographyResets}
 ${typography}
 ${mediaQueries}
