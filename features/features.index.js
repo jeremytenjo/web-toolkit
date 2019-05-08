150ce24a51fc80661af70185b66d0e7fd4181579
Jeremy <tenjojeremy@gmail.com>
2019-05-08 08:42:07 -0700

    chaned features index to easier export

--- features/features.index.js
+++ features/features.index.js
@@ -1,10 +1,5 @@
-import Input from './Forms/Ui/Input/Input.index'
-import Chat from './Comunication/Chat/Component/chat.index'
-import {
-  ShareInit,
-  ShareIsSupported,
-} from './Engagement/WebApi/Share/share.index'
-import NavBar from './Navigation/Navbar/Component/navBar.index'
-import Calendar from './Dates/Calendar/calendar.index'
-
-export { Input, Chat, ShareInit, ShareIsSupported, NavBar, Calendar }
+export * from './Forms/Ui/Input/Input.index'
+export * from './Comunication/Chat/Component/chat.index'
+export * from './Engagement/WebApi/Share/share.index'
+export * from './Navigation/Navbar/Component/navBar.index'
+export * from './Dates/Calendar/calendar.index'
