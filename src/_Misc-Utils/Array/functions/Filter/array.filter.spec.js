eb3d76ab57ed31f9ad612035e2abd1bcb69baa55
Jeremy Tenjo <tenjojeremy@gmail.com>
2019-05-27 09:52:12 -0700

    Update array.filter.spec.js
    Changed description

--- src/_Misc-Utils/Array/functions/Filter/array.filter.spec.js
+++ src/_Misc-Utils/Array/functions/Filter/array.filter.spec.js
@@ -1,6 +1,6 @@
 import filter from './array.filter'
 
-test('should Filter array of strings', () => {
+test('should Filter array of single values', () => {
   const arrayOfStrings = ['1', '2', '3']
   const result = filter({
     array: arrayOfStrings,
@@ -10,16 +10,5 @@ test('should Filter array of strings', () => {
   })
   expect(result).toContain('3')
 })
-
-// test('should Filter array of strings and numbers', () => {
-//   const arrayOfStrings = ['1', '2', 3]
-//   const result = filter({
-//     array: arrayOfStrings,
-//     post: '',
-//     condition: '===',
-//     value: 3,
-//   })
-//   expect(result).toContain('3')
-// })
 
 // should Filter array of objects
