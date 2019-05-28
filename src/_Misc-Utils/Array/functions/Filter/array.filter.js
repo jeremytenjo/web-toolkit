fac719b5fd7ac663cbc3e545c90776fe2c9354f9
Jeremy Tenjo <tenjojeremy@gmail.com>
2019-05-27 17:01:51 -0700

    Update array.filter.js
    changed string conditionn to operator

--- src/_Misc-Utils/Array/functions/Filter/array.filter.js
+++ src/_Misc-Utils/Array/functions/Filter/array.filter.js
@@ -3,7 +3,7 @@ import dynamicCondition from '../../../Operator/dynamicOperator'
 export default function({ array, post, condition, value }) {
   // handle array of single values
   const result = array.filter((item) => {
-    return dynamicCondition({post: item, condition, value})
+    return dynamicCondition({post: item, operator: condition, value})
   })
 
   return result
