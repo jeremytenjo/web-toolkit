4a78e305fc9444ad22c62e03d7943f869a88127e
Jeremy <tenjojeremy@gmail.com>
2019-05-29 12:54:42 -0700

    added bl0ck to chat storie

--- src/Comunication/Chat/Examples/chat.stories.js
+++ src/Comunication/Chat/Examples/chat.stories.js
@@ -2,8 +2,20 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withReadme } from 'storybook-readme'
 
+import B from '../../../../.storybook/Custom-Components/VariationBlock/variationBlock.index'
 import README from '../README.md'
 
 import Default from './Default/chat.default.example'
 
-storiesOf('Chat', module).add('default', withReadme(README, () => <Default />))
+const Variations = () => (
+  <>
+    <B title='Default'>
+      <Default />
+    </B>
+  </>
+)
+
+storiesOf('Chat', module).add(
+  'default',
+  withReadme(README, () => <Variations />),
+)
