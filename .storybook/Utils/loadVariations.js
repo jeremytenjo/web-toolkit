ca5c2bcabb0b66363ff67387ea5197c3e703d98e
Jeremy <tenjojeremy@gmail.com>
2019-05-31 06:03:17 -0700

    add lad varoatins example

---
+++ .storybook/Utils/loadVariations.js
@@ -0,0 +1,28 @@
+// Usage
+
+// const [Variation, setVariation] = useState(null)
+// const req = require.context('./', true, /\.stories\.js$/)
+
+// useEffect(() => {
+//   loadStories()
+// }, [])
+
+// const loadStories = () => {
+//   req.keys().forEach(async (filename, i) => {
+//     const Comp = await import(`${filename}`)
+//     console.log(filename)
+
+//     if (i === 0) {
+//       console.log(i)
+
+//       console.log(Comp)
+//       setVariation(Comp.default)
+
+//       // return req(filename)
+//       return Comp
+//     }
+//   })
+// }
+// return (
+//   <>
+//     {Variation && <Variation />}
