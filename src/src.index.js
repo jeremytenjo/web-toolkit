57eece0900ea16883c8493543fce937fd0c3e702
Jeremy <tenjojeremy@gmail.com>
2019-06-02 18:55:05 -0700

    added style2 navbar

--- src/src.index.js
+++ src/src.index.js
@@ -1,6 +1,6 @@
 import Chat from './Comunication/Chat/Component/chat.index'
 import { ShareInit, ShareIsSupported } from './Engagement/Share/share.index'
-import NavBar from './Navigation/Navbar/Ui/React/navBar.index'
+import NavBar from './Navigation/Navbar/Ui/React/Style1/navBar.index'
 import Calendar from './Dates/Calendar/calendar.index'
 import List from './Data-Display/List/Ui/React/list.index'
 import Image from './Media/Image/Ui/React/image.index'
