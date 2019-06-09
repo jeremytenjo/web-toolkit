6b4a820659e25e58a71074d52c4d4c435c6e7898
Jeremy <tenjojeremy@gmail.com>
2019-06-09 15:16:28 -0700

    ixed stye1 export

--- src/Input/Button/Ui/React/Styles/Style1/style1.index.js
+++ src/Input/Button/Ui/React/Styles/Style1/style1.index.js
@@ -4,7 +4,7 @@ import Button from '../../button.index'
 
 import { Wrapper } from './style1.styles'
 
-export default ({ props }) => (
+export default (props) => (
   <>
     <Button Wrapper={Wrapper} {...props} />
   </>
