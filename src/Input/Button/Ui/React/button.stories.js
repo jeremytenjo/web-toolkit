801822ec933076ef5c0896d47eb1f106902c37a5
Jeremy <tenjojeremy@gmail.com>
2019-06-09 14:33:58 -0700

    added button variations

--- src/Input/Button/Ui/React/button.stories.js
+++ src/Input/Button/Ui/React/button.stories.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import B from '../../../../../.storybook/Custom-Components/VariationBlock/variationBlock.index'
+
 import Button from './button.index'
 
-storiesOf('Input|Button/Ui/React', module)
-  .add('Primary', () => <Button color='primary' />)
-  .add('Secondary', () => <Button color='secondary' />)
-  .add('Active', () => <Button active />)
-  .add('Disabled', () => <Button disabled />)
-  .add('Outlined', () => <Button outlined />)
-  .add('Outlined', () => <Button outlined />)
-  .add('with Icon', () => <Button iconName='camera' />)
-  .add('FAB', () => <Button iconName='camera' type='FAB' />)
+const Variations = () => {
+  return (
+    <>
+      <B title='color - primary'>
+        <Button color='primary' />
+      </B>
+      <B title='color - secondary'>
+        <Button color='secondary' />
+      </B>
+      <B title='active'>
+        <Button active />
+      </B>
+      <B title='disabled'>
+        <Button disabled />
+      </B>
+      <B title='outlined' color='black'>
+        <Button outlined />
+      </B>
+      <B title='iconName'>
+        <Button iconName='camera' />
+      </B>
+      <B title='FAB'>
+        <Button iconName='camera' type='FAB' />
+      </B>
+    </>
+  )
+}
+
+storiesOf('Input|Button/Ui/React', module).add('Variations', () => (
+  <Variations />
+))
