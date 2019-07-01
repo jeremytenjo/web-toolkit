b33d8758d03c2375638de09b8e37caf3cde491f0
Jeremy <tenjojeremy@gmail.com>
2019-06-30 17:30:18 -0700

    Illustration: fixed story

--- src/Data-Display/Illustration/Ui/React/illustration.stories.js
+++ src/Data-Display/Illustration/Ui/React/illustration.stories.js
@@ -1,30 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { storiesOf } from '@storybook/react'
 
 import B from '../../../../../.storybook/Custom-Components/VariationBlock/variationBlock.index'
 import List from '../../../List/Ui/React/list.index'
 
-import Illustration from './illustration.index'
-
 const illustrationsReq = require.context('./', true, /.js$/)
 const illustrationsPaths = illustrationsReq.keys()
 
-const getIconName = (paths) =>
-  paths.map((icon) => {
-    icon = icon.substring(0, icon.length - 3)
-    icon = icon.replace('./', '')
-    const iconArray = icon.split('.')
-    const iconLength = iconArray.length
+const ignorePaths = [
+  'illustration.index',
+  'illustration.styles',
+  'illustration.stories',
+]
 
-    return (
-      <B key={icon} title={icon} noBackground style={{ width: '400px' }}>
-        {icon !== './illustrations.stories' && iconLength === 1 ? (
-          <Illustration name={icon} size={200} />
-        ) : null}
-      </B>
-    )
-  })
-const Illustrations = () => <List grid>{getIconName(illustrationsPaths)}</List>
+const Illustrations = () => {
+  const [vars, setvars] = useState([])
+  useEffect(() => {
+    getIconName(illustrationsPaths)
+  }, [])
+
+  const getIconName = async (paths) => {
+    const helo0 = paths.map(async (icon) => {
+      icon = icon.substring(0, icon.length - 3)
+      icon = icon.replace('./', '')
+      const iconArray = icon.split('.')
+      const iconLength = iconArray.length
+
+      let Ill = await import(`./${icon}.js`)
+      Ill = Ill.default
+
+      return !ignorePaths.includes(icon) && Ill && iconLength === 1 ? (
+        <B key={icon} title={icon} noBackground>
+          <Ill />
+        </B>
+      ) : null
+    })
+
+    setvars(await Promise.all(helo0))
+  }
+
+  return <List>{vars}</List>
+}
 
 storiesOf('Data-Display|Illustrations/Ui/React', module).add(
   'collection',
