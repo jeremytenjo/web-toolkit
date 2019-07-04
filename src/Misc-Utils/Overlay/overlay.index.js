1364525408e1a825eea5bff0e3074a28d020b52d
Jeremy Tenjo <jeremy.tenjo.fcvinteractive.com>
2019-07-04 16:16:07 -0700

    Overlay: scroll

--- src/Misc-Utils/Overlay/overlay.index.js
+++ src/Misc-Utils/Overlay/overlay.index.js
@@ -41,17 +41,8 @@ const Overlay = ({
     if (disableScrollOnShow) show ? disableScroll() : enableScroll()
   }, [show])
 
-  const disableScroll = () => {
-    document.body.style.position = 'fixed'
-    document.body.style.top = `-${window.scrollY}px`
-  }
-
-  const enableScroll = () => {
-    const top = document.body.style.top
-    document.body.style.position = ''
-    document.body.style.top = ''
-    window.scrollTo(0, parseInt(scrollY || '0') * -1)
-  }
+  const disableScroll = () => (document.body.style.overflow = 'hidden')
+  const enableScroll = () => (document.body.style.overflow = 'auto')
 
   const handleKeyInput = ({ key }) => key === 'Escape' && onClick()
 
