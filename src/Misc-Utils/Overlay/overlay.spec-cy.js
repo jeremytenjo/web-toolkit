c2f999d3eda59644b618fb347ef269c1a85ba8ca
Jeremy <tenjojeremy@gmail.com>
2019-07-05 16:48:22 -0700

    OverlayL added teast

--- src/Misc-Utils/Overlay/overlay.spec-cy.js
+++ src/Misc-Utils/Overlay/overlay.spec-cy.js
@@ -1,15 +1,15 @@
-context('Actions', () => {
+context('Overlay', () => {
   beforeEach(() => {
     cy.visit(
-      'http://localhost:9002/?path=/story/misc-utils-overlay-ui-react--variations',
+      'http://localhost:9002/iframe.html?id=misc-utils-overlay-ui-react--variations',
     )
   })
 
   it('shows on button click & hides on overlay click', function() {
-    cy.get('data-cy=overlay').should('not.be.visible')
-    cy.get('data-cy=show-button').click()
-    cy.get('data-cy=overlay').should('be.visible')
-    cy.get('data-cy=overlay').click()
-    cy.get('data-cy=overlay').should('not.be.visible')
+    cy.get('[data-cy="overlay"]').should('not.be.visible')
+    cy.get('button[data-cy="show-button"]').click()
+    cy.get('[data-cy="overlay"]').should('be.visible')
+    cy.get('[data-cy="overlay"]').click()
+    cy.get('[data-cy="overlay"]').should('not.be.visible')
   })
 })
