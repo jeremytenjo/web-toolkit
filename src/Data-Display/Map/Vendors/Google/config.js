3bee25f5f8bd3944cd13610e8db5fd6064bd8ae4
Jeremy <tenjojeremy@gmail.com>
2019-07-11 08:16:39 -0700

    f

--- src/Data-Display/Map/Vendors/Google/config.js
+++ src/Data-Display/Map/Vendors/Google/config.js
@@ -1,4 +1,3 @@
-const PlacesAPI =
-  'https://maps.googleapis.com/maps/api/js?key=AIzaSyCXzV8qXUuwPo_6TkZX4bmiqCgIgm8IjOc&libraries=places'
+const PlacesAPI = 'https://maps.googleapis.com/maps/api/js?key=<APIKEY>'
 
 export default PlacesAPI
