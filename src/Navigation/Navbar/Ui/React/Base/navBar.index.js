976e4c97595f5b074e44087e4cc9c6b9a77e143c
Jeremy <tenjojeremy@gmail.com>
2019-07-16 21:44:49 -0700

    Navbar: add image feature

--- src/Navigation/Navbar/Ui/React/Base/navBar.index.js
+++ src/Navigation/Navbar/Ui/React/Base/navBar.index.js
@@ -2,16 +2,19 @@ import React, { memo } from 'react'
 
 import List from '../../../../../Data-Display/List/Ui/React/list.index'
 import Icon from '../../../../../Data-Display/Icon/Ui/React/icon.index'
+import Image from '../../../../../Media/Image/Ui/React/image.index'
 
 import { defaultProps, propTypes } from './navBar.propTypes'
 import { Wrapper } from './navBar.styles'
 
-const NavBar = ({ data, color }) => {
+const NavBar = ({ data, color, onInput }) => {
   return (
     <Wrapper>
       <List style={{ justifyContent: 'space-around' }}>
-        {data.map(({ icon, label, link }) => {
-          return (
+        {data.map(({ file = null, src = null, icon, label, link }) => {
+          const inputProps = file ? { onInput } : null
+
+          return icon ? (
             <Icon
               key={label}
               name={icon}
@@ -19,8 +22,16 @@ const NavBar = ({ data, color }) => {
               color={color}
               link={link}
               noBackground
+              inputProps={inputProps}
             />
-          )
+          ) : src ? (
+            <Image
+              src={src}
+              round
+              borderColor={color}
+              inputProps={inputProps}
+            />
+          ) : null
         })}
       </List>
     </Wrapper>
--- src/Navigation/Navbar/Ui/React/Base/navBar.links.js
+++ src/Navigation/Navbar/Ui/React/Base/navBar.links.js
@@ -15,8 +15,8 @@ export default [
     url: 'explore',
   },
   {
-    label: 'Explore',
-    icon: 'camera/material',
-    type: 'file',
+    src:
+      'https://firebasestorage.googleapis.com/v0/b/estilistta.appspot.com/o/hayle.jpeg?alt=media&token=b8eb9208-6456-4075-9f57-c2c680745604',
+    file: true,
   },
 ]
