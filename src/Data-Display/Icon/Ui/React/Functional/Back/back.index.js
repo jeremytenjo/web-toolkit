c97c7842ce2ca663e2effa5cb63be7d4495e6513
Jeremy <tenjojeremy@gmail.com>
2019-08-18 12:35:36 -0600

    fixed router.goBack

--- src/Data-Display/Icon/Ui/React/Functional/Back/back.index.js
+++ src/Data-Display/Icon/Ui/React/Functional/Back/back.index.js
@@ -10,7 +10,7 @@ const IconBack = ({
 }) => {
   // Functions
   const goBack = () => {
-    to ? router.history.push(to) : router.goBack()
+    to ? router.history.push(to) : router.history.goBack()
     onGoBack()
   }
 
