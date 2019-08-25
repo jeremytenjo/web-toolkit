201a15082ef447063ab2c6d194211a2dda95eb30
Jeremy <tenjojeremy@gmail.com>
2019-08-25 16:20:42 -0600

    Animation: added blinking type

---
+++ src/Misc-Utils/Animations/Web-Animations-API/Types/animation.blinking.js
@@ -0,0 +1,24 @@
+// Keyframes should be set before used in animate()
+export default ({ el, config, show }) => {
+  const keyframes = [
+    {
+      opacity: show ? 1 : 0,
+    },
+    {
+      opacity: show ? 0 : 1,
+    },
+    {
+      opacity: show ? 1 : 0,
+    },
+  ]
+
+  el.style.display = 'block'
+  const anim = el.animate(keyframes, {
+    ...config,
+    iterations: Infinity,
+    duration: 1000,
+  })
+  anim.onfinish = () => {
+    if (!show) el.style.display = 'none'
+  }
+}
