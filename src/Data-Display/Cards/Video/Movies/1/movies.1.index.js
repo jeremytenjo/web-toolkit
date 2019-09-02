bfe38815910a5fbc48c258359525a4ce674e2116
tenjojeremy <tenjojeremy@gmail.com>
2019-09-02 11:16:02 -0600

    Card  Movie" upaed border radius

--- src/Data-Display/Cards/Video/Movies/1/movies.1.index.js
+++ src/Data-Display/Cards/Video/Movies/1/movies.1.index.js
@@ -36,7 +36,7 @@ const CardMovie1 = ({
   return (
     <Box
       backgroundColor='white'
-      borderRadius='20px'
+      borderRadius='10px'
       gridAutoFlow='column'
       gridGap='s'
       padding='s'
