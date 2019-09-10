d9918b00884f488ad814725a19c22588403d1521
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-09-10 15:10:59 -0600

    Select: added title

--- src/Input/Form/Select/Ui/React/Styles/select.tvguide.js
+++ src/Input/Form/Select/Ui/React/Styles/select.tvguide.js
@@ -1,23 +1,30 @@
 import React, { memo } from 'react'
 
 import Box from '../../../../../../Data-Display/Box/Ui/React/box.index'
+import Typogrgraphy from '../../../../../../Data-Display/Typography/Ui/React/typography.index'
 import SelectBase from '../Base/select.index'
 
 const Item = ({
   label,
   backgroundColor = 'primary',
   foregroundColor = 'primary-darker',
+  fontColor = 'black',
+  title = 'seasons',
 }) => {
   return (
-    <Box
-      size={43}
-      backgroundColor={backgroundColor}
-      color={foregroundColor}
-      borderRadius='100px'
-      justifyItems='center'
-      alignItems='center'
-    >
-      {label}
+    <Box justifyItems='center' gridGap='s'>
+      <Box
+        size={43}
+        backgroundColor={backgroundColor}
+        color={foregroundColor}
+        borderRadius='100px'
+        justifyItems='center'
+        alignItems='center'
+      >
+        {label}
+      </Box>
+
+      <Typogrgraphy text={title} color={fontColor} />
     </Box>
   )
 }
