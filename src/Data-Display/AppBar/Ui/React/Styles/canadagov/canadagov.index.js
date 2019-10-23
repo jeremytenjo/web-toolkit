7145326d116022d729993e6c514d566691b16c88
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-10-23 12:02:35 -0600

    appbar canadagov : carot is optional

--- src/Data-Display/AppBar/Ui/React/Styles/canadagov/canadagov.index.js
+++ src/Data-Display/AppBar/Ui/React/Styles/canadagov/canadagov.index.js
@@ -22,18 +22,20 @@ const AppbarCangov = ({
   <Box {...WrapperStyles} {...wrapperProps}>
     <Box {...TypographyWrapperStyles} onClick={onClick} role='button'>
       <Typography text={title} {...typographyProps} />
-      <svg
-        width='11'
-        height='9'
-        viewBox='0 0 11 9'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path
-          d='M5.5 9L0.73686 0.750001L10.2631 0.75L5.5 9Z'
-          fill={`var(--color-${typographyProps.color})`}
-        />
-      </svg>
+      {typographyProps.carot && (
+        <svg
+          width='11'
+          height='9'
+          viewBox='0 0 11 9'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M5.5 9L0.73686 0.750001L10.2631 0.75L5.5 9Z'
+            fill={`var(--color-${typographyProps.color})`}
+          />
+        </svg>
+      )}
     </Box>
 
     <Image src={logo} {...ImageStyles} {...imageProps} />
