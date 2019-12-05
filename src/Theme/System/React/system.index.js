30fefc80c9e571f1567a6482dcd805d4775eb673
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-04 19:11:31 -0700

    clean

--- src/Theme/System/React/system.index.js
+++ src/Theme/System/React/system.index.js
@@ -48,7 +48,11 @@ export default (props) => {
       key.includes('Gap')
     )
       array.push(getString({ varName: 'spacing', key, value }))
-    else if (key.includes('color') || key.includes('Color')|| key.includes('fill'))
+    else if (
+      key.includes('color') ||
+      key.includes('Color') ||
+      key.includes('fill')
+    )
       array.push(getString({ varName: 'color', key, value }))
     else if (key.includes('shadow') || key.includes('Shadow'))
       array.push(getString({ varName: 'boxShadow', key, value }))
