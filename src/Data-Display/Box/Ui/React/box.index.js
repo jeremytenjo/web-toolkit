54bbbb39883c269445de33755514e378945fe483
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-05 20:51:07 -0700

    Box: added rest props

--- src/Data-Display/Box/Ui/React/box.index.js
+++ src/Data-Display/Box/Ui/React/box.index.js
@@ -3,13 +3,14 @@ import React, { memo } from 'react'
 import { defaultProps, propTypes } from './box.propTypes'
 import { Wrapper } from './box.styles'
 
-const Box = ({ children, name, styles, fullscreen, size }) => {
+const Box = ({ children, name, styles, fullscreen, size, ...rest }) => {
   return (
     <Wrapper
       styles={{ display: 'grid', padding: 'xs', ...styles }}
       data-box-name={name}
       fullscreen={fullscreen}
       size={size}
+      {...rest}
     >
       {children}
     </Wrapper>
