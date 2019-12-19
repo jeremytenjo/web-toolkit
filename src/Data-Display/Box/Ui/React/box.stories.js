2582cae2d2bf4ce83c3d80ff0e57ed8c1bc6cfe9
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-19 08:51:10 -0700

    Box: story updates syles

--- src/Data-Display/Box/Ui/React/box.stories.js
+++ src/Data-Display/Box/Ui/React/box.stories.js
@@ -10,7 +10,14 @@ import Box from './box.index'
 const Variations = () => (
   <>
     <B title='default' background style={{ width: '400px', padding: 0 }}>
-      <Box justifyContent='space-between' gridAutoFlow={['column', 'row']} width='100%'>
+      <Box
+        styles={{
+          justifyContent: 'space-between',
+          gridAutoFlow: ['column', 'row'],
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
         <h4>Featured</h4>
         <Icon name='search/material' color='primary' />
       </Box>
