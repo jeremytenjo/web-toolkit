8d017c8931fd24ec52d32cf3269fc4ed05527c6b
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-20 17:47:08 -0700

    notifications: updated firebase version is sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js')
 
 firebase.initializeApp({
+  apiKey: 'AIzaSyAr0XrSoBuZP8_fcvJKnjSD3YfDpdzr0kQ',
+  authDomain: 'tenjo-web-toolkit.firebaseapp.com',
+  databaseURL: 'https://tenjo-web-toolkit.firebaseio.com',
+  projectId: 'tenjo-web-toolkit',
+  storageBucket: 'tenjo-web-toolkit.appspot.com',
   messagingSenderId: '761012518504',
+  appId: '1:761012518504:web:4cbe27f5fe948d41',
 })
 
 const messaging = firebase.messaging()
