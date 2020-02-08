c53e18a93d42c21166e769547eded17d0d7c8b73
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-08 12:47:43 -0500

    version bump

--- src/Engagement/Share/Examples/share.mockData.js
+++
@@ -1,9 +0,0 @@
-// Main
-const ShareMockData = {
-  title: 'My awesome post!',
-  text: 'This post may or may not contain the answer to the universe',
-  url: window.location.href,
-}
-
-// Exports
-export default ShareMockData
--- src/Engagement/Share/share.index.js
+++
@@ -1,4 +0,0 @@
-import ShareInit from './functions/share.init'
-import ShareIsSupported from './functions/share.isSupported'
-
-export { ShareInit, ShareIsSupported }
