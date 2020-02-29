a05cc8dc3db8be57f5a209080c76f057591816fe
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-29 17:53:39 -0500

    Icons: removed sizes i

--- src/dataDisplay/icon/library/github/1.js
+++ src/dataDisplay/icon/library/github/1.js
@@ -3,8 +3,6 @@ import React from 'react'
 export default () => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='512'
-    height='512'
     x='0'
     y='0'
     enableBackground='new 0 0 512 512'
