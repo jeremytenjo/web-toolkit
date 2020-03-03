28392ec2c87491edae06761306219e3c51667913
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-03 07:54:40 -0500

    particles: dfuized proptyes

--- src/ambient/particles/propTypes.js
+++ src/ambient/particles/propTypes.js
@@ -1,11 +1,6 @@
-import { obj } from 'prop-types'
-
 export const defaultProps = {
   color: { value: '#B5FFFC' },
   params: {},
 }
 
-export const propTypes = {
-  color: obj,
-  text: obj,
-}
+export const propTypes = {}
