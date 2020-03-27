263f6bfa7550cd922a4169a438e2f9b05438f6eb
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-27 16:39:46 -0400

    sspec

---
+++ src/utils/array/functions/removeDuplicates/remvoecommetnslaterspec.js
@@ -0,0 +1,8 @@
+// import func from './'
+
+// // Features
+// test('Check whether at least one element in the array passes the test implemented by the provided function - Single Values', () => {
+//   const data = [{ title: 'hello' }, { title: 'hello' }]
+//   const result = func({ data, comp: 'title' })
+//   expect(result).toEqual([{ title: 'hello' }])
+// })
