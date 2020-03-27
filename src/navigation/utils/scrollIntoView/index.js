1018a5083ea46931622c264685c9b60b0b595f08
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-27 10:16:09 -0400

    navigation

--- src/navigation/utils/scrollIntoView/index.js
+++ src/navigation/utils/scrollIntoView/index.js
@@ -1,5 +1,5 @@
 export default (id, config = { behavior: 'smooth' }) => {
-  if (!id) return throw Error('add a selector as the first parameter')
+  if (!id) return throw new Error('add a selector as the first parameter')
 
   const element = document.querySelector(id)
   element.scrollIntoView(config)
