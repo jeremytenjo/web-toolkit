7c328c225c2cb6c33bc2745318a97cdb6070b11e
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-27 10:40:14 -0400

    scrollintoview: return console log error

--- src/navigation/utils/scrollIntoView/index.js
+++ src/navigation/utils/scrollIntoView/index.js
@@ -1,5 +1,6 @@
 export default (id, config = { behavior: 'smooth' }) => {
-  if (!id) return throw new Error('add a selector as the first parameter')
+  if (!id)
+    return console.error('scrollIntoView error - add a selector as the first parameter')
 
   const element = document.querySelector(id)
   element.scrollIntoView(config)
