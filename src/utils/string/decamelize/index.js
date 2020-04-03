357111aaefee360c17099988e4af81735d44d4aa
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-04-03 10:12:59 -0400

    String: updaed story

--- src/utils/string/decamelize/index.js
+++ src/utils/string/decamelize/index.js
@@ -4,11 +4,13 @@
  * @param str String in camelcase
  * @param separator Separator for the new decamelized string.
  */
-export default (str, separator) => {
-  separator = typeof separator === 'undefined' ? '_' : separator
+export default (str, separator = '-') => {
+  if (!str) return console.error(`str is ${str} , add string as first parameter`)
 
   return str
     .replace(/([a-z\d])([A-Z])/g, `$1${separator}$2`)
     .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, `$1${separator}$2`)
     .toLowerCase()
 }
+
+export const testValue = 'marginRight'
