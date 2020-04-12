fb67ea0ab3cb913610e24c6e4d3cff80af9fd88c
jtenjo <jtenjo@criticalmass.com>
2020-04-12 15:28:49 -0400

    clean

--- .storybook/generators/component/templates/stories/utils/createImportString.js
+++ .storybook/generators/component/templates/stories/utils/createImportString.js
@@ -1,5 +1,4 @@
 module.exports = (name, nameUppercase, base = '@tenjojeremy/web-toolkit/', path) => {
-  console.log(__dirname)
   let importString = `import ${nameUppercase} from '${base}${path}/styles/1'`
 
   return importString
